dcbf5d82c998fe1fbb9c5e21da0c12b60ae58395
loelio <loelio@gmail.com>
2024-05-01 19:13:10 +0000

    Added Backup Storage module

--- main.tf
+++ main.tf
@@ -201,10 +201,12 @@ resource "null_resource" "configure-cat-app" {
     }
     
   }
+
   
-  module "azure-backup" {
-    source  = "ravensorb/azure-backup/azurerm"
-    version = "1.0.2"
-    resource_group_name = "${var.prefix}-public"
-  }
+}
+
+module "azure-backup" {
+  source  = "ravensorb/azure-backup/azurerm"
+  version = "1.0.2"
+  resource_group_name = "${var.prefix}-public"
 }
